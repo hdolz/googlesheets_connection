04a50381f13cddc44fae453022d81bb89da1da3d
Henrique Dolzane Mota <henrique.dolzane@bemol.local>
2019-09-24 16:17:19 -0400

    Commit inicial

---
+++ app.js
@@ -0,0 +1,40 @@
+// API Rest para conexão com Google SpreadSheet API 
+const express = require('express');
+const GoogleSpreadSheet = require('google-spreadsheet');
+const { promisify } = require('util');
+const bodyParser = require('body-parser');
+const creds = require('./credenciais.json');
+
+const app = express();
+const porta = 5000;
+
+//middleware do body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Index');
+});
+
+app.post('/inserir', (req, res) => {
+    (async function inserirDados() {
+        const doc = new GoogleSpreadSheet('1BjVQnZsan3PnRZK5HyThB1-UmnrpT8XuHA9Vgv7VGn0');
+        await promisify(doc.useServiceAccountAuth)(creds);
+        const info = await promisify(doc.getInfo)()
+        const sheet = info.worksheets[0];
+        await promisify(sheet.addRow)({
+            // Nome: req.body.Nome,
+            // Area: req.body.Area,
+            // Curso: req.body.Curso
+            Nome: req.body.Nome,
+            Email: req.body.Email,
+            DtNasc: req.body.DtNasc,
+            Interesse: req.body.Interesse
+        });
+        res.send('Dados inseridos com sucesso!');
+    })()
+});
+
+app.listen(porta, () => {
+    console.log(`Servidor ouvindo na porta ${porta}`);
+});
